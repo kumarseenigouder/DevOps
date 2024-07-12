7c054163739da9b2f706226dd5b371f6096baa1e
KUMAR <kumarseenigouder@gmail.com>
2024-07-12 15:04:29 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,5 @@
-int x = 5;
-int y = 6;
-int sum = x + y;
-System.out.println(sum); // Print the sum of x + y
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
